f7d670d5382f96ecc573b3a5d05ee8650aca2f4f
alinashaihutdinova <shayhutdinovaal.06@gmail.com>
2025-05-27 20:56:19 +0300

    добавлена папка ресурсов

--- Checkers/Forms/EntranceForm.Designer.cs
+++ Checkers/Forms/EntranceForm.Designer.cs
@@ -43,9 +43,9 @@
             lblTitle.BackColor = Color.FromArgb(65, 35, 20);
             lblTitle.Font = new Font("Arial", 24F, FontStyle.Regular, GraphicsUnit.Point, 204);
             lblTitle.ForeColor = Color.FromArgb(226, 199, 153);
-            lblTitle.Location = new Point(350, 77);
+            lblTitle.Location = new Point(350, 90);
             lblTitle.Name = "lblTitle";
-            lblTitle.Size = new Size(368, 55);
+            lblTitle.Size = new Size(515, 55);
             lblTitle.TabIndex = 0;
             lblTitle.Text = "Шашки Онлайн";
             lblTitle.TextAlign = ContentAlignment.MiddleCenter;
@@ -55,9 +55,9 @@
             txtLogin.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             txtLogin.Font = new Font("Arial Rounded MT Bold", 14F);
             txtLogin.ForeColor = Color.Black;
-            txtLogin.Location = new Point(330, 160);
+            txtLogin.Location = new Point(330, 200);
             txtLogin.Name = "txtLogin";
-            txtLogin.Size = new Size(400, 40);
+            txtLogin.Size = new Size(547, 40);
             txtLogin.TabIndex = 1;
             txtLogin.Text = "Логин";
             // 
@@ -66,9 +66,9 @@
             txtPassword.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             txtPassword.Font = new Font("Arial", 14F, FontStyle.Bold);
             txtPassword.ForeColor = Color.Black;
-            txtPassword.Location = new Point(330, 220);
+            txtPassword.Location = new Point(330, 260);
             txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(370, 40);
+            txtPassword.Size = new Size(517, 40);
             txtPassword.TabIndex = 2;
             txtPassword.Text = "Пароль";
             txtPassword.Enter += txtPassword_Enter;
@@ -80,25 +80,24 @@
             btnTogglePassword.BackColor = SystemColors.Window;
             btnTogglePassword.FlatAppearance.BorderSize = 0;
             btnTogglePassword.FlatStyle = FlatStyle.Flat;
-            btnTogglePassword.Location = new Point(700, 221);
+            btnTogglePassword.Location = new Point(829, 260);
             btnTogglePassword.Name = "btnTogglePassword";
-            btnTogglePassword.Size = new Size(30, 38);
+            btnTogglePassword.Size = new Size(48, 40);
             btnTogglePassword.TabIndex = 3;
-            btnTogglePassword.Text = "👁";
             btnTogglePassword.UseVisualStyleBackColor = false;
             btnTogglePassword.Click += btnTogglePassword_Click;
             // 
             // btnLogin
             // 
             btnLogin.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            btnLogin.BackColor = Color.DarkGreen;
+            btnLogin.BackColor = Color.FromArgb(0, 64, 0);
             btnLogin.FlatAppearance.BorderSize = 0;
             btnLogin.FlatStyle = FlatStyle.Flat;
             btnLogin.Font = new Font("Arial", 14F, FontStyle.Regular, GraphicsUnit.Point, 204);
             btnLogin.ForeColor = Color.White;
-            btnLogin.Location = new Point(330, 290);
+            btnLogin.Location = new Point(450, 370);
             btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(400, 40);
+            btnLogin.Size = new Size(300, 50);
             btnLogin.TabIndex = 4;
             btnLogin.Text = "Войти";
             btnLogin.UseVisualStyleBackColor = false;
@@ -107,14 +106,14 @@
             // btnRegister
             // 
             btnRegister.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            btnRegister.BackColor = Color.DarkGreen;
+            btnRegister.BackColor = Color.FromArgb(0, 64, 0);
             btnRegister.FlatAppearance.BorderSize = 0;
             btnRegister.FlatStyle = FlatStyle.Flat;
             btnRegister.Font = new Font("Arial", 14F, FontStyle.Regular, GraphicsUnit.Point, 204);
             btnRegister.ForeColor = Color.White;
-            btnRegister.Location = new Point(330, 350);
+            btnRegister.Location = new Point(450, 450);
             btnRegister.Name = "btnRegister";
-            btnRegister.Size = new Size(400, 40);
+            btnRegister.Size = new Size(300, 50);
             btnRegister.TabIndex = 5;
             btnRegister.Text = "Зарегистрироваться";
             btnRegister.UseVisualStyleBackColor = false;
@@ -124,17 +123,17 @@
             // 
             lightpanel.Anchor = AnchorStyles.None;
             lightpanel.BackColor = Color.FromArgb(65, 35, 20);
-            lightpanel.Location = new Point(166, 60);
+            lightpanel.Location = new Point(221, 56);
             lightpanel.Name = "lightpanel";
-            lightpanel.Size = new Size(737, 438);
+            lightpanel.Size = new Size(770, 493);
             lightpanel.TabIndex = 6;
             // 
             // EntranceForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
-            AutoScaleMode = AutoScaleMode.Font;
+            AutoScaleDimensions = new SizeF(144F, 144F);
+            AutoScaleMode = AutoScaleMode.Dpi;
             BackColor = Color.FromArgb(40, 20, 0);
-            ClientSize = new Size(1059, 571);
+            ClientSize = new Size(1206, 635);
             Controls.Add(btnRegister);
             Controls.Add(btnLogin);
             Controls.Add(btnTogglePassword);
@@ -142,8 +141,10 @@
             Controls.Add(txtLogin);
             Controls.Add(lblTitle);
             Controls.Add(lightpanel);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "EntranceForm";
             Text = "EntranceForm";
+            Load += EntranceForm_Load;
             ResumeLayout(false);
             PerformLayout();
         }
