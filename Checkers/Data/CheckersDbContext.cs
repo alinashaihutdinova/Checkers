93eae69b2235fb1d471fca37359bc6637f0219c9
Ulyana Novoselova <uliana.a.novoselova@gmail.com>
2025-06-05 22:25:42 +0300

    добавила sql файл

---
+++ Checkers/Data/CheckersDbContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Checkers.Core.Entities;
+
+namespace Checkers.Data
+{
+    /// <summary>
+    /// контекст базы данных
+    /// </summary>
+    public class CheckersDbContext : DbContext
+    {
+        /// <summary>
+        /// таблица "users" в БД
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+        /// <summary>
+        /// таблица "games" в БД
+        /// </summary>
+        public DbSet<Game> Games { get; set; }
+        /// <summary>
+        /// таблица "moves" в БД
+        /// </summary>
+        public DbSet<Move> Moves { get; set; }
+        /// <summary>
+        /// таблица "GameHistories" в БД
+        /// </summary>
+        public DbSet<GameHistory> GameHistories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5434;Database=mydatabase;Username=postgres;Password=alshnqq");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            
+            modelBuilder.Entity<Game>()
+            .HasOne(g => g.WhitePlayer)
+            .WithMany(u => u.WhiteGames)
+            .HasForeignKey(g => g.WhitePlayerId)
+            .OnDelete(DeleteBehavior.Restrict);
+            
+            modelBuilder.Entity<Game>()
+            .HasOne(g => g.BlackPlayer)
+            .WithMany(u => u.BlackGames)
+            .HasForeignKey(g => g.BlackPlayerId)
+            .OnDelete(DeleteBehavior.Restrict);
+           
+            modelBuilder.Entity<GameHistory>()
+            .HasOne(h => h.User)
+            .WithMany(u => u.GameHistories)
+            .HasForeignKey(h => h.UserId);
+
+            modelBuilder.Entity<GameHistory>()
+                .HasOne(h => h.Game)
+                .WithMany()
+                .HasForeignKey(h => h.GameId);
+        }
+    }
+}
